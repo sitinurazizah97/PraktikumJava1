bac8ed78a59f425f445c8da28e4e96c498254528
sitinurazizah97 <sitinurazizah7597@gmail.com>
2020-04-24 10:45:45 +0800

    deklarasi variabel dan tipe data

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes{
+ public static void main(String[] args){
+
+  String namaDepan = "Siti";
+  String namaTengah = "Nur";
+  String namaBelakang = "Azizah";
+  int usia = 22;
+  int targetTahunKuliah = 4;
+  double ipk = 3.89764512;
+  char nilaiAbjad = 'A';
+  boolean cantik = true;
+
+ }
+}
