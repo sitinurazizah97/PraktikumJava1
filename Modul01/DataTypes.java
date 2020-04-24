4ca27b79e0ec0b5f040db733d175ec42e534d237
sitinurazizah97 <sitinurazizah7597@gmail.com>
2020-04-24 11:02:54 +0800

    Output dengan sout

--- Modul01/DataTypes.java
+++ Modul01/DataTypes.java
@@ -9,6 +9,14 @@ public class DataTypes{
   double ipk = 3.89764512;
   char nilaiAbjad = 'A';
   boolean cantik = true;
-
+  
+  System.out.println("Nama depan : " + namaDepan);
+  System.out.println("Nama tengah : " + namaTengah);
+  System.out.println("Nama belakang : " + namaBelakang);
+  System.out.println("Usia : " + usia);
+  System.out.println("Target Kuliah : " + targetTahunKuliah + " tahun");
+  System.out.println("IPK : " + ipk);
+  System.out.println("Nilai PBO : " + nilaiAbjad);
+  System.err.println("Cantik : " + cantik);
  }
 }
