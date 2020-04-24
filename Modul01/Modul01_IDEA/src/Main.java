6d1d54b108025e6d077fae4d73aadb931c8c0b0a
sitinurazizah97 <sitinurazizah7597@gmail.com>
2020-04-24 11:58:03 +0800

    Project IDEA

---
+++ Modul01/Modul01_IDEA/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Siti";
+        String namaTengah = "Nur";
+        String namaBelakang = "Azizah";
+        int usia = 22;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean cantik = true;
+
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+    }
+}
