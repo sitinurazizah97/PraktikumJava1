254471274f1f4dc0880d5e1ab2345efdd32f39e0
sitinurazizah97 <sitinurazizah7597@gmail.com>
2020-04-24 12:14:54 +0800

    Operasi Tipe String

--- Modul01/Modul01_IDEA/src/Main.java
+++ Modul01/Modul01_IDEA/src/Main.java
@@ -10,5 +10,25 @@ public class Main {
         boolean cantik = true;
 
         System.out.println(namaDepan + namaTengah + namaBelakang);
+
+        char[] uniskaChar = {'U','N','I','S','K','A'};
+        String namaLengkap = namaDepan + " " +
+                namaTengah + " " + namaBelakang;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5,12));
+        System.out.println(namaLengkap.replace("Yog","Barr"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split( " ");
+
+        for(String nama : namaArray){
+            System.out.println(nama);
+        }
+
     }
 }
+
